d4e6e145dc627224f0e68ef2065ed805dbc27ab1
beland.dev <beland.dev>
2022-06-24 16:59:52 +0700

    update

--- src/project/projectInfo.ts
+++ src/project/projectInfo.ts
@@ -10,32 +10,8 @@ export type ProjectInfo = {
 export function getProjectInfo(workDir: string): ProjectInfo {
   const assetJsonPath = path.resolve(workDir, 'asset.json')
   if (fs.existsSync(assetJsonPath)) {
-    try {
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      const assetJson = require(assetJsonPath)
-      if (sdk.AssetJson.validate(assetJson)) {
-        if (assetJson.assetType === sdk.ProjectType.PORTABLE_EXPERIENCE) {
-          return {
-            sceneId: assetJson.id,
-            sceneType: sdk.ProjectType.PORTABLE_EXPERIENCE
-          }
-        }
-      } else {
-        const errors = (sdk.AssetJson.validate.errors || [])
-          .map((a) => `${a.dataPath} ${a.message}`)
-          .join('')
-
-        console.error(
-          `Unable to validate asset.json properly, please check it.`,
-          errors
-        )
-      }
-    } catch (err) {
-      console.error(`Unable to load asset.json properly, please check it.`, err)
-    }
-
     return {
-      sceneId: '',
+      sceneId: 'b64-' + Buffer.from(workDir).toString('base64'),
       sceneType: sdk.ProjectType.SMART_ITEM
     }
   }
